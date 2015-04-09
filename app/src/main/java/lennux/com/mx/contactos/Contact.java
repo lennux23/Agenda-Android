986dc1e3295ee152f639fbfd9566b525e65925dd
MobileStudio06 <mobilestudio06@iMacdeMStudio06.lan>
2015-04-08 20:23:32 -0500

    agregando redes sociales

--- app/src/main/java/lennux/com/mx/contactos/Contact.java
+++ app/src/main/java/lennux/com/mx/contactos/Contact.java
@@ -15,23 +15,29 @@ public class Contact {
     private String mail;
     private String phone;
     private Drawable picture;
+    private String github;
+    private String twitter;
+    private String facebook;
 
-    public Contact(String name, String mail, String phone, Drawable picture) {
+    public Contact(String name, String mail, String phone, Drawable picture, String github, String twitter, String facebook) {
         this.name = name;
         this.mail = mail;
         this.phone = phone;
         this.picture = picture;
+        this.github = github;
+        this.twitter = twitter;
+        this.facebook = facebook;
     }
 
     public static ArrayList<Contact> getContacts (Context context) {
 
         ArrayList<Contact> arrayContact= new ArrayList<Contact>();
 
-        arrayContact.add(new Contact("Juan Carlos","jcmtza611@gmail.com","5591988868",context.getResources().getDrawable(R.mipmap.pic_juan)));
-        arrayContact.add(new Contact("Christian Jesus Rodriguez Martinez","jesusrdzmtzm@gmail.com","5538753188",context.getResources().getDrawable(R.mipmap.pic_chris)));
-        arrayContact.add(new Contact("Alejandro Xochihua Jorge","alexochihua23@gmail.com","5560116967",context.getResources().getDrawable(R.mipmap.pic_alex)));
-        arrayContact.add(new Contact("Ricardo Centeno Lugo","ricardo.celj@gmail.com","5514382887",context.getResources().getDrawable(R.mipmap.pic_richar)));
-        arrayContact.add(new Contact("Genaro Rodriguez","grodriguezm@redcibercom.com.mx","5528982474",context.getResources().getDrawable(R.mipmap.pic_genaro)));
+        arrayContact.add(new Contact("Juan Carlos","jcmtza611@gmail.com","5591988868",context.getResources().getDrawable(R.mipmap.pic_juan),null,null,null));
+        arrayContact.add(new Contact("Christian Jesus Rodriguez Martinez","jesusrdzmtzm@gmail.com","5538753188",context.getResources().getDrawable(R.mipmap.pic_chris), "jesusrdzmtzm", "jesusrdzmtzm","1154932578"));
+        arrayContact.add(new Contact("Alejandro Xochihua Jorge","alexochihua23@gmail.com","5560116967",context.getResources().getDrawable(R.mipmap.pic_alex), "lennux23", "axj_lennux@hotmail.com", "100000972018165"));
+        arrayContact.add(new Contact("Ricardo Centeno Lugo","ricardo.celj@gmail.com","5514382887",context.getResources().getDrawable(R.mipmap.pic_richar),null,null,null));
+        arrayContact.add(new Contact("Genaro Rodriguez","grodriguezm@redcibercom.com.mx","5528982474",context.getResources().getDrawable(R.mipmap.pic_genaro),null,null,null));
 
         return arrayContact;
 
@@ -67,6 +73,30 @@ public class Contact {
 
     public void setPicture(Drawable picture) {
         this.picture = picture;
+    }
+
+    public String getGithub() {
+        return github;
+    }
+
+    public void setGithub(String github) {
+        this.github = github;
+    }
+
+    public String getTwitter() {
+        return twitter;
+    }
+
+    public void setTwitter(String twitter) {
+        this.twitter = twitter;
+    }
+
+    public String getFacebook() {
+        return facebook;
+    }
+
+    public void setFacebook(String facebook) {
+        this.facebook = facebook;
     }
 
 }
--- app/src/main/java/lennux/com/mx/contactos/ContactDetailActivity.java
+++ app/src/main/java/lennux/com/mx/contactos/ContactDetailActivity.java
@@ -1,17 +1,28 @@
 package lennux.com.mx.contactos;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
 
 
-public class ContactDetailActivity extends ActionBarActivity {
+public class ContactDetailActivity extends ActionBarActivity implements View.OnClickListener{
 
     private int contactPosition;
     private Contact contact;
+    ImageButton actionCall, actionEmail, actionGithub, actionTwitter, actionFacebook;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,10 +39,30 @@ public class ContactDetailActivity extends ActionBarActivity {
         TextView tvMailDetail = (TextView) findViewById(R.id.txtMailDetail);
         TextView tvPhoneDetail = (TextView) findViewById(R.id.txtPhoneDetail);
 
+        TextView tvGithubDetail = (TextView) findViewById(R.id.txtGithubDetail);
+        TextView tvTwitterDetail = (TextView) findViewById(R.id.txtTwitterDetail);
+        TextView tvFacebookDetail = (TextView) findViewById(R.id.txtFacebookDetail);
+
         profileImage.setImageDrawable(contact.getPicture());
         tvNameDetail.setText(contact.getName());
         tvMailDetail.setText(contact.getMail());
         tvPhoneDetail.setText(contact.getPhone());
+        tvGithubDetail.setText(contact.getGithub());
+        tvTwitterDetail.setText(contact.getTwitter());
+        tvFacebookDetail.setText(contact.getFacebook());
+
+        actionCall = (ImageButton) findViewById(R.id.actionCall);
+        actionEmail = (ImageButton) findViewById(R.id.actionMail);
+        actionGithub = (ImageButton) findViewById(R.id.actionGithub);
+        actionTwitter = (ImageButton) findViewById(R.id.actionTwitter);
+        actionFacebook = (ImageButton) findViewById(R.id.actionFacebook);
+
+        actionCall.setOnClickListener(this);
+        actionEmail.setOnClickListener(this);
+        actionGithub.setOnClickListener(this);
+        actionTwitter.setOnClickListener(this);
+        actionFacebook.setOnClickListener(this);
+
     }
 
 
@@ -55,5 +86,75 @@ public class ContactDetailActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.actionCall:
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                String uri = "tel:" + contact.getPhone();
+
+                callIntent.setData(Uri.parse(uri));
+                if (isAvailable(getApplicationContext(), callIntent))
+                    startActivity(callIntent);
+                else
+                    Toast.makeText(getApplicationContext(), "HARWARE NOT AVAILABLE", Toast.LENGTH_LONG).show();
+
+                break;
+            case R.id.actionMail:
+                Log.e("INETNT", "email");
+                Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
+                        "mailto", contact.getMail(), null));
+                emailIntent.putExtra(Intent.EXTRA_SUBJECT, "EXTRA_SUBJECT");
+
+                if(isAvailable(getApplicationContext(),emailIntent))
+                    startActivity(Intent.createChooser(emailIntent, "Send email..."));
+                else
+                    Toast.makeText(getApplicationContext(),"HARWARE NOT AVAILABLE", Toast.LENGTH_LONG).show();
+
+                break;
+            case R.id.actionGithub:
+                Intent gihubIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(
+                        "http://www.github.com/"+ contact.getGithub()));
+
+                if(isAvailable(getApplicationContext(),gihubIntent))
+                    startActivity(gihubIntent);
+                else
+                    Toast.makeText(getApplicationContext(),"HARWARE NOT AVAILABLE", Toast.LENGTH_LONG).show();
+
+                break;
+            case R.id.actionTwitter:
+                Intent twitterIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(
+                        "twitter://user?screen_name="+ contact.getTwitter()));
+
+                if(isAvailable(getApplicationContext(),twitterIntent))
+                    startActivity(twitterIntent);
+                else
+                    Toast.makeText(getApplicationContext(),"HARWARE NOT AVAILABLE", Toast.LENGTH_LONG).show();
+
+                break;
+            case R.id.actionFacebook:
+                Intent faceIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(
+                        "fb://profile/" +contact.getFacebook()));
+
+                if(isAvailable(getApplicationContext(),faceIntent))
+                    startActivity(faceIntent);
+                else
+                    Toast.makeText(getApplicationContext(),"HARWARE NOT AVAILABLE", Toast.LENGTH_LONG).show();
+
+                break;
+        }
+
+
+    }
+
+
+    public static boolean isAvailable(Context ctx, Intent intent) {
+        final PackageManager mgr = ctx.getPackageManager();
+        List<ResolveInfo> list =
+                mgr.queryIntentActivities(intent,
+                        PackageManager.MATCH_DEFAULT_ONLY);
+        return list.size() > 0;
     }
 }
